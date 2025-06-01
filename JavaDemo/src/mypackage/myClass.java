5825741e56cb34dea50fd96d1dd18d60157302b4
Prashant Bhaskar Gharat <39966175+pawas17@users.noreply.github.com>
2025-06-01 17:55:42 +0530

    Developed application for java code  demo

---
+++ JavaDemo/src/mypackage/myClass.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class myClass {
+       
+	  public int add(int n1, int n2)
+	      {
+	       return(n1+n2);
+	      }
+	  public static int Subtract(int n1, int n2)
+      {
+       return(n1-n2);
+      }
+}
